85bd5fc00abb0ad681be2862c26afe5c9d3ba198
Franz Andrei M. Cruspero <franz.cruspero@gmail.com>
2020-02-27 02:21:13 +0800

    Django App Components - complete

--- src/try_django/urls.py
+++ src/try_django/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, re_path
 
+from blog.views import (
+    blog_post_detail_page
+)
+
 from .views import (
     home_page,
     about_page,
@@ -23,8 +27,10 @@ from .views import (
     example_page,
 )
 
+
 urlpatterns = [
     path('', home_page),
+    path('blog/', blog_post_detail_page),
     path('about/', about_page),
     re_path(r'^pages?/$', about_page), #-> this is equivalent to path('pages/', about_page) and path('page/', about_page)
     # re_path(r'^about/$', about_page),
